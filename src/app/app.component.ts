3e1b60d4481e456a8b350c2c86f5c79544b2dbcc
atobaros <aleho100@gmail.com>
2021-11-25 01:01:21 +0200

    adelantando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'square-votes';
+
+  public x:number = 10;
+  public theDate:Date = new Date();
+   
+  public buttonVal:string = 'click me mathafacka'; 
+
+  public changeText(){
+    this.buttonVal = 'you clicked me ben zona';
+  }
+
+  public color:string = "background-color:purple";
+
+  nameChange(event:Event){
+    let name = event.target;
+    console.log(name);
+    (<HTMLInputElement>name).innerHTML = "pompilio";
+    
+  }
+
 }
